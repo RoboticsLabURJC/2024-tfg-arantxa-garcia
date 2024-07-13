bce4350c71010349f748cda9adb72c16372f54c0
Arantxa <a.garciab.2021@alumnos.urjc.es>
2024-07-13 14:35:22 +0200

    semana 4-6

--- src/videohands.py
+++ src/videohands.py
@@ -1,94 +1,251 @@
 import cv2
 import mediapipe as mp
 import sys
+import numpy as np
+import json
+import time
 
-def process_video(video_path):
-    mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose()
-    mp_hands = mp.solutions.hands
-    hands = mp_hands.Hands()
-    mp_drawing = mp.solutions.drawing_utils
+class VideoProcessor:
+    def __init__(self, video_path, json_path, draw_pose=False, draw_hands=False, draw_face=False, pose_colors=None):
+        self.video_path = video_path
+        self.json_path = json_path
+        self.draw_pose = draw_pose
+        self.draw_hands = draw_hands
+        self.draw_face = draw_face
 
-    pose_colors = { # colores para las partes del cuerpo
-        'LEFT_ARM': (255, 0, 0),
-        'RIGHT_ARM': (0, 255, 0),
-        'LEFT_LEG': (0, 0, 255),
-        'RIGHT_LEG': (255, 255, 0),
-        'TORSO': (255, 0, 255),
-        'HEAD': (0, 255, 255)
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose()
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands()
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = self.mp_face_mesh.FaceMesh()
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.drawing_spec = self.mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-    }
+        default_pose_colors = {
+            'LEFT_ARM': (255, 0, 0),
+            'RIGHT_ARM': (0, 255, 0),
+            'LEFT_LEG': (0, 0, 255),
+            'RIGHT_LEG': (255, 255, 0),
+            'TORSO': (255, 0, 255),
+            'HEAD': (0, 255, 255)
+        }
 
-    cap = cv2.VideoCapture(video_path)
+        self.pose_colors = pose_colors if pose_colors is not None else default_pose_colors
+        self.actions = self.load_actions_from_json()
 
-    if not cap.isOpened():
-        print(f"No se pudo abrir el video: {video_path}")
-        return
+    def calculate_center_of_mass(self, landmarks):
+        x_coords = [landmark.x for landmark in landmarks]
+        y_coords = [landmark.y for landmark in landmarks]
 
-    while cap.isOpened():
-        success, frame = cap.read()
-        if not success:
-            break
+        center_x = np.mean(x_coords)
+        center_y = np.mean(y_coords)
 
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # open cv lee en BGR, y el video es RGB
+        return center_x, center_y
 
-        results_pose = pose.process(frame_rgb) # detectamos la pose y las manos
-        results_hands = hands.process(frame_rgb) 
-
-        if results_pose.pose_landmarks: # pintamos la pose en el frame
-
-            for connection in mp_pose.POSE_CONNECTIONS:
+    def process_pose(self, frame, results_pose):
+        if results_pose.pose_landmarks:
+            for connection in self.mp_pose.POSE_CONNECTIONS:
                 start_idx = connection[0]
                 end_idx = connection[1]
 
-                if start_idx in mp_pose.PoseLandmark.__members__.values() and end_idx in mp_pose.PoseLandmark.__members__.values():
+                if start_idx in self.mp_pose.PoseLandmark.__members__.values() and end_idx in self.mp_pose.PoseLandmark.__members__.values():
                     start_landmark = results_pose.pose_landmarks.landmark[start_idx]
                     end_landmark = results_pose.pose_landmarks.landmark[end_idx]
 
-                    # Determinar el color basado en la parte del cuerpo
                     if start_idx in [11, 13, 15, 17, 19, 21] and end_idx in [11, 13, 15, 17, 19, 21]:
-                        color = pose_colors['LEFT_ARM']
+                        color = self.pose_colors['LEFT_ARM']
                     elif start_idx in [12, 14, 16, 18, 20, 22] and end_idx in [12, 14, 16, 18, 20, 22]:
-                        color = pose_colors['RIGHT_ARM']
+                        color = self.pose_colors['RIGHT_ARM']
                     elif start_idx in [23, 25, 27, 29, 31] and end_idx in [23, 25, 27, 29, 31]:
-                        color = pose_colors['LEFT_LEG']
+                        continue
+                        color = self.pose_colors['LEFT_LEG']
                     elif start_idx in [24, 26, 28, 30, 32] and end_idx in [24, 26, 28, 30, 32]:
-                        color = pose_colors['RIGHT_LEG']
+                        continue
+                        color = self.pose_colors['RIGHT_LEG']
                     elif start_idx in [11, 12, 24, 23] and end_idx in [11, 12, 24, 23]:
-                        color = pose_colors['TORSO']
+                        color = self.pose_colors['TORSO']
                     else:
-                        color = pose_colors['HEAD']
+                        continue
+                        color = self.pose_colors['HEAD']
 
                     start_point = (int(start_landmark.x * frame.shape[1]), int(start_landmark.y * frame.shape[0]))
                     end_point = (int(end_landmark.x * frame.shape[1]), int(end_landmark.y * frame.shape[0]))
                     cv2.line(frame, start_point, end_point, color, 2)
 
-            # mp_drawing.draw_landmarks(
-            #     frame,
-            #     results_pose.pose_landmarks,
-            #     mp_pose.POSE_CONNECTIONS,
-            #     landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=1, circle_radius=2)
-            # )
-
-        if results_hands.multi_hand_landmarks: # pintamos las manos en el frame
+    def process_hands(self, frame, results_hands):
+        if results_hands.multi_hand_landmarks:
             for hand_landmarks in results_hands.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
+                self.mp_drawing.draw_landmarks(
                     frame, 
                     hand_landmarks, 
-                    mp_hands.HAND_CONNECTIONS
+                    self.mp_hands.HAND_CONNECTIONS
                 )
 
-        cv2.imshow('MediaPipe Hands + Pose', frame)
+                center_x, center_y = self.calculate_center_of_mass(hand_landmarks.landmark)
+                center_point = (int(center_x * frame.shape[1]), int(center_y * frame.shape[0]))
 
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
+                cv2.circle(frame, center_point, 20, (255, 128, 64), -1)
 
-    cap.release()
-    cv2.destroyAllWindows()
+    def process_face(self, frame, results_face):
+        if results_face.multi_face_landmarks:
+            for face_landmarks in results_face.multi_face_landmarks:
+                self.mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=self.mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=self.drawing_spec,
+                    connection_drawing_spec=self.drawing_spec
+                )
+
+    def process_video(self):
+        cap = cv2.VideoCapture(self.video_path)
+
+        if not cap.isOpened():
+            print(f"No se pudo abrir el video: {self.video_path}")
+            return
+
+        frame_number = 0
+        while cap.isOpened():
+            success, frame = cap.read()
+            if not success:
+                break
+
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            results_pose = self.pose.process(frame_rgb)
+            results_hands = self.hands.process(frame_rgb)
+            results_face = self.face_mesh.process(frame_rgb)
+
+            if self.draw_pose:
+                self.process_pose(frame, results_pose)
+            if self.draw_hands:
+                self.process_hands(frame, results_hands)
+            if self.draw_face:
+                self.process_face(frame, results_face)
+
+            self.print_action_for_frame(frame, frame_number)
+
+            cv2.imshow('MediaPipe Hands + Pose + Face', frame)
+
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+
+            frame_number += 1
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def combine_videos(self, video_paths):
+        caps = [cv2.VideoCapture(video_path) for video_path in video_paths]
+
+        if not all([cap.isOpened() for cap in caps]):
+            print("No se pudo abrir uno o más videos.")
+            return
+
+        width = int(caps[0].get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(caps[0].get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = caps[0].get(cv2.CAP_PROP_FPS)
+
+        # Reduce the size of each frame
+        reduced_width = width // 2
+        reduced_height = height // 2
+        output_size = (reduced_width * 2, reduced_height * 2)
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter('combined_video.mp4', fourcc, fps, output_size)
+
+        frame_number = 0
+        while all([cap.isOpened() for cap in caps]):
+            frames = []
+            for cap in caps:
+                success, frame = cap.read()
+                if not success:
+                    break
+                # Resize each frame to the reduced dimensions
+                frame = cv2.resize(frame, (reduced_width, reduced_height))
+                frames.append(frame)
+
+            if len(frames) != len(caps):
+                break
+
+            combined_frame = np.zeros((output_size[1], output_size[0], 3), dtype=np.uint8)
+
+            combined_frame[0:reduced_height, 0:reduced_width] = frames[0]
+            if len(frames) > 1:
+                combined_frame[0:reduced_height, reduced_width:reduced_width*2] = frames[1]
+            if len(frames) > 2:
+                combined_frame[reduced_height:reduced_height*2, 0:reduced_width] = frames[2]
+
+            # Create a blank space in the bottom-right quadrant to display actions
+            if frame_number in self.actions:
+                actions = self.actions[frame_number]
+                for i, action in enumerate(actions):
+                    cv2.putText(combined_frame, action, (reduced_width + 10, reduced_height + 30 + i * 30), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+
+            cv2.imshow('Combined Video', combined_frame)
+            out.write(combined_frame)
+
+            time.sleep(0.02)
+
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+
+            frame_number += 1
+
+        for cap in caps:
+            cap.release()
+        out.release()
+        cv2.destroyAllWindows()
+
+    def load_actions_from_json(self):
+        try:
+            with open(self.json_path, 'r', encoding='utf-8-sig') as f:
+                data = json.load(f)
+        except UnicodeDecodeError:
+            print(f"Failed to decode JSON file {self.json_path}. Trying alternative encoding...")
+            with open(self.json_path, 'r', encoding='latin-1') as f:
+                data = json.load(f)
+        
+        actions = {}
+        
+        for frame_id, frame_data in data["openlabel"]["actions"].items():
+            if "type" in frame_data:
+                for frame_interval in frame_data["frame_intervals"]:
+                    frame_start = frame_interval["frame_start"]
+                    frame_end = frame_interval["frame_end"]
+                    for frame in range(frame_start, frame_end + 1):
+                        if frame not in actions:
+                            actions[frame] = []
+                        if frame_data["type"] not in actions[frame]:
+                            actions[frame].append(frame_data["type"])
+        
+        return actions
+
+    def print_action_for_frame(self, frame, frame_number):
+        if frame_number in self.actions:
+            actions = self.actions[frame_number]
+            for i, action in enumerate(actions):
+                cv2.putText(frame, action, (10, 30 + i * 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("falta el video cazurro")
+    if len(sys.argv) < 2:
+        print("Falta el json y el path del video.")
     else:
-        video_path = sys.argv[1]
-        process_video(video_path)
+        json_path = sys.argv[1]
+        video_path = sys.argv[2]
+        draw_pose = "--pose" in sys.argv
+        draw_hands = "--hands" in sys.argv
+        draw_face = "--face" in sys.argv
+        combine_videos = "--combine" in sys.argv
+
+        processor = VideoProcessor(video_path, json_path, draw_pose=draw_pose, draw_hands=draw_hands, draw_face=draw_face)
+        
+        if combine_videos:
+            if len(sys.argv) < 6:
+                print("Faltan rutas de videos para combinar.")
+            else:
+                video_paths = sys.argv[2:5]
+                processor.combine_videos(video_paths)
+        else:
+            processor.process_video()
