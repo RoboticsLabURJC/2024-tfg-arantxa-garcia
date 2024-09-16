cbb6729e9bc0c750a01355639279dbe4b46f374a
Arantxa <a.garciab.2021@alumnos.urjc.es>
2024-09-16 09:59:29 +0200

    mirando su ya funciona

---
+++ src/videohands.py
@@ -0,0 +1,343 @@
+import cv2
+import mediapipe as mp
+import sys
+import numpy as np
+import json
+import time
+import csv
+
+class VideoProcessor:
+    def __init__(self, video_path, json_path, draw_pose=False, draw_hands=False, draw_face=False, pose_colors=None):
+        self.video_path = video_path
+        self.json_path = json_path
+        self.draw_pose = draw_pose
+        self.draw_hands = draw_hands
+        self.draw_face = draw_face
+
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose()
+        self.mp_hands = mp.solutions.hands # con uno vale para ambos videos ?????????????????????????????????????????????????????????
+        # self.hands = self.mp_hands.Hands()
+        self.hands_pose = mp.solutions.hands.Hands()  # Para el video de pose
+        self.hands_only = mp.solutions.hands.Hands()  # Para el video de hands
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = self.mp_face_mesh.FaceMesh()
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.drawing_spec = self.mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+        self.face_sync = 0
+        self.hands_sync = 0
+        self.pose_sync = 0
+
+        default_pose_colors = {
+            'LEFT_ARM': (255, 0, 0),
+            'RIGHT_ARM': (0, 255, 0),
+            'LEFT_LEG': (0, 0, 255),
+            'RIGHT_LEG': (255, 255, 0),
+            'TORSO': (255, 0, 255),
+            'HEAD': (0, 255, 255)
+        }
+
+        self.pose_colors = pose_colors if pose_colors is not None else default_pose_colors
+        self.actions = self.load_actions_from_json()
+
+    def calculate_center_of_mass(self, landmarks):
+        x_coords = [landmark.x for landmark in landmarks]
+        y_coords = [landmark.y for landmark in landmarks]
+
+        center_x = np.mean(x_coords)
+        center_y = np.mean(y_coords)
+
+        return center_x, center_y
+
+    def process_pose(self, frame, results_pose):
+        if results_pose.pose_landmarks:
+            for connection in self.mp_pose.POSE_CONNECTIONS:
+                start_idx = connection[0]
+                end_idx = connection[1]
+
+                if start_idx in self.mp_pose.PoseLandmark.__members__.values() and end_idx in self.mp_pose.PoseLandmark.__members__.values():
+                    start_landmark = results_pose.pose_landmarks.landmark[start_idx]
+                    end_landmark = results_pose.pose_landmarks.landmark[end_idx]
+
+                    if start_idx in [11, 13, 15, 17, 19, 21] and end_idx in [11, 13, 15, 17, 19, 21]:
+                        color = self.pose_colors['LEFT_ARM']
+                    elif start_idx in [12, 14, 16, 18, 20, 22] and end_idx in [12, 14, 16, 18, 20, 22]:
+                        color = self.pose_colors['RIGHT_ARM']
+                    elif start_idx in [23, 25, 27, 29, 31] and end_idx in [23, 25, 27, 29, 31]:
+                        continue
+                        color = self.pose_colors['LEFT_LEG']
+                    elif start_idx in [24, 26, 28, 30, 32] and end_idx in [24, 26, 28, 30, 32]:
+                        continue
+                        color = self.pose_colors['RIGHT_LEG']
+                    elif start_idx in [11, 12, 24, 23] and end_idx in [11, 12, 24, 23]:
+                        color = self.pose_colors['TORSO']
+                    else:
+                        continue
+                        color = self.pose_colors['HEAD']
+
+                    start_point = (int(start_landmark.x * frame.shape[1]), int(start_landmark.y * frame.shape[0]))
+                    end_point = (int(end_landmark.x * frame.shape[1]), int(end_landmark.y * frame.shape[0]))
+                    cv2.line(frame, start_point, end_point, color, 2)
+
+    def process_hands(self, frame, results_hands):
+        if results_hands.multi_hand_landmarks:
+            for hand_landmarks in results_hands.multi_hand_landmarks:
+                self.mp_drawing.draw_landmarks(
+                    frame, 
+                    hand_landmarks, 
+                    self.mp_hands.HAND_CONNECTIONS
+                )
+
+                center_x, center_y = self.calculate_center_of_mass(hand_landmarks.landmark)
+                center_point = (int(center_x * frame.shape[1]), int(center_y * frame.shape[0]))
+
+                cv2.circle(frame, center_point, 20, (255, 128, 64), -1)
+
+    def process_face(self, frame, results_face):
+        if results_face.multi_face_landmarks:
+            for face_landmarks in results_face.multi_face_landmarks:
+                self.mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=self.mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=self.drawing_spec,
+                    connection_drawing_spec=self.drawing_spec
+                )
+
+    def process_video(self): # EN PROCESO, AUN NO FUNCIONA
+        cap = cv2.VideoCapture(self.video_path)
+
+        if not cap.isOpened():
+            print(f"No se pudo abrir el video: {self.video_path}")
+            return
+
+        frame_data = []
+        frame_number = 0
+        while cap.isOpened():
+            success, frame = cap.read()
+            if not success:
+                break
+
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            results_pose = self.pose.process(frame_rgb)
+            results_hands = self.hands.process(frame_rgb)
+            results_face = self.face_mesh.process(frame_rgb)
+
+            if self.draw_pose:
+                self.process_pose(frame, results_pose)
+            if self.draw_hands:
+                self.process_hands(frame, results_hands)
+            if self.draw_face:
+                self.process_face(frame, results_face)
+
+            # Agregar información de este frame
+            actions = self.actions.get(frame_number, [])
+            frame_data.append({
+                'frame_number': frame_number,
+                'actions': ', '.join(actions),
+                'pose_sync': self.pose_sync,
+                'hands_sync': self.hands_sync,
+                'face_sync': self.face_sync
+            })
+
+            self.print_action_for_frame(frame, frame_number)
+
+            cv2.imshow('MediaPipe Hands + Pose + Face', frame)
+
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+
+            frame_number += 1
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+        # Guardar datos en un CSV después de procesar el video
+        self.get_csv(frame_data)
+
+    def combine_videos(self, video_paths):
+        caps = [cv2.VideoCapture(video_path) for video_path in video_paths]
+
+        if not all([cap.isOpened() for cap in caps]):
+            print("No se pudo abrir uno o más videos.")
+            return
+
+        width = int(caps[0].get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(caps[0].get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = caps[0].get(cv2.CAP_PROP_FPS)
+
+        # Reduce the size of each frame
+        reduced_width = width // 2
+        reduced_height = height // 2
+        output_size = (reduced_width * 2, reduced_height * 2)
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter('combined_video.mp4', fourcc, fps, output_size)
+
+        frame_number = 0
+        video_started = [False, False, False]  # Flags to track when each video starts
+
+        while all([cap.isOpened() for cap in caps]):
+            frames = []
+            cap_number = 0
+            for cap in caps:
+                # If the video has already started, read it normally
+                if video_started[cap_number]:
+                    success, frame = cap.read()
+                    if not success:
+                        break
+                else:
+                    # If it has not started, it pauses in black until the synchronization is complete
+                    frame = np.zeros((height, width, 3), dtype=np.uint8)  # Frame negro
+
+                black_frame = np.zeros((reduced_height, reduced_width, 3), dtype=np.uint8)
+
+                if cap_number == 0:  # First video (pose)
+                    if frame_number >= self.pose_sync:
+                        if not video_started[cap_number]:  # Since frame 0
+                            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                            video_started[cap_number] = True  # Marks the video as started
+
+                        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                        results_pose = self.pose.process(frame_rgb)
+                        results_hands = self.hands_pose.process(frame_rgb)
+                        self.process_pose(frame, results_pose)
+                        self.process_hands(frame, results_hands) 
+                        frame = cv2.resize(frame, (reduced_width, reduced_height))
+                    else:
+                        frame = black_frame  # Black till synchronization of pose_sync
+
+                elif cap_number == 1:  # Second video (hands)
+                    if frame_number >= self.hands_sync:
+                        if not video_started[cap_number]: 
+                            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                            video_started[cap_number] = True 
+
+                        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                        results_hands = self.hands_only.process(frame_rgb)
+                        self.process_hands(frame, results_hands)
+                        frame = cv2.resize(frame, (reduced_width, reduced_height))
+                    else:
+                        frame = black_frame  # Black till synchronization of hands_sync
+
+                elif cap_number == 2:  # Third video (face)
+                    if frame_number >= self.face_sync:
+                        if not video_started[cap_number]:
+                            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                            video_started[cap_number] = True  
+
+                        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                        results_face = self.face_mesh.process(frame_rgb)
+                        self.process_face(frame, results_face)
+                        frame = cv2.resize(frame, (reduced_width, reduced_height))
+                    else:
+                        frame = black_frame  # Black till synchronization of face_sync
+
+                frames.append(frame)
+                cap_number += 1
+
+            if len(frames) != len(caps):
+                break
+
+            combined_frame = np.zeros((output_size[1], output_size[0], 3), dtype=np.uint8)
+
+            # Each frame in its quadrant
+            combined_frame[0:reduced_height, 0:reduced_width] = frames[0]  # First video
+            if len(frames) > 1:
+                combined_frame[0:reduced_height, reduced_width:reduced_width*2] = frames[1]  # Second video
+            if len(frames) > 2:
+                combined_frame[reduced_height:reduced_height*2, 0:reduced_width] = frames[2]  # Third video
+
+            # Blank space for actions
+            if frame_number in self.actions:
+                actions = self.actions[frame_number]
+                for i, action in enumerate(actions):
+                    cv2.putText(combined_frame, action, (reduced_width + 10, reduced_height + 30 + i * 30), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+
+            cv2.imshow('Combined Video', combined_frame)
+            out.write(combined_frame)
+
+            if cv2.waitKey(1) & 0xFF == 27:  
+                break
+
+            frame_number += 1
+
+        for cap in caps:
+            cap.release()
+        out.release()
+        cv2.destroyAllWindows()
+
+    def load_actions_from_json(self):
+        try:
+            with open(self.json_path, 'r', encoding='utf-8-sig') as f:
+                data = json.load(f)
+        except UnicodeDecodeError:
+            print(f"Failed to decode JSON file {self.json_path}. Trying alternative encoding...")
+            with open(self.json_path, 'r', encoding='latin-1') as f:
+                data = json.load(f)
+        
+        actions = {}
+        
+        for frame_id, frame_data in data["openlabel"]["actions"].items():
+            if "type" in frame_data:
+                for frame_interval in frame_data["frame_intervals"]:
+                    frame_start = frame_interval["frame_start"]
+                    frame_end = frame_interval["frame_end"]
+                    for frame in range(frame_start, frame_end + 1):
+                        if frame not in actions:
+                            actions[frame] = []
+                        if frame_data["type"] not in actions[frame]:
+                            actions[frame].append(frame_data["type"])
+
+        for frame_id, frame_data in data["openlabel"]["streams"].items():
+            if "face_camera" in frame_id:
+                self.face_sync = frame_data["stream_properties"]["sync"]["frame_shift"]
+                print("Face sync: ", self.face_sync)
+            elif "hands_camera" in frame_id:
+                self.hands_sync = frame_data["stream_properties"]["sync"]["frame_shift"]
+                print("Hands sync: ", self.hands_sync)
+            elif "body_camera" in frame_id:
+                self.pose_sync = frame_data["stream_properties"]["sync"]["frame_shift"]
+                print("Pose sync: ", self.pose_sync)
+                    
+        return actions
+
+    def print_action_for_frame(self, frame, frame_number):
+        if frame_number in self.actions:
+            actions = self.actions[frame_number]
+            for i, action in enumerate(actions):
+                cv2.putText(frame, action, (10, 30 + i * 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+
+    def get_csv(self, frame_data):
+        csv_file = 'video_frame_data.csv'
+        with open(csv_file, mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['frame_number', 'actions', 'pose_sync', 'hands_sync', 'face_sync'])
+            writer.writeheader()
+            for data in frame_data:
+                writer.writerow(data)
+        print(f"Datos guardados en {csv_file}")
+        
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Falta el json y el path del video.")
+    else:
+        json_path = sys.argv[1]
+        video_path = sys.argv[2]
+        draw_pose = "--pose" in sys.argv
+        draw_hands = "--hands" in sys.argv
+        draw_face = "--face" in sys.argv
+        combine_videos = "--combine" in sys.argv
+
+        processor = VideoProcessor(video_path, json_path, draw_pose=draw_pose, draw_hands=draw_hands, draw_face=draw_face)
+        
+        if combine_videos:
+            if len(sys.argv) < 6:
+                print("Faltan rutas de videos para combinar.")
+            else:
+                video_paths = sys.argv[2:5]
+                processor.combine_videos(video_paths)
+        else:
+            processor.process_video()
